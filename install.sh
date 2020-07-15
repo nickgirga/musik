0890c39ef3cc0bd56f7ffdb449e028aa17b96768
nickgirga <nickgirga@gmail.com>
2020-07-15 01:49:56 -0700

    added a prompt for scale-to-fit

--- install.sh
+++ install.sh
@@ -2,4 +2,13 @@
 cd _build/
 meson --reconfigure
 meson install
-scale-to-fit musik on
+
+while true; do
+ read -p 'Do you wish you enable scale-to-fit? [Y/n]: ' response
+ echo
+ case $response in
+  [Yy]* ) scale-to-fit musik on; break;;
+  [Nn]* ) exit;;
+  * ) echo "Please answer \"Y\", \"y\", \"N\", or \"n\"."
+ esac
+done
